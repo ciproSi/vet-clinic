e55bcba56cef1407b811b057cddfe243468fbe25
Adam-Lnenicka <70516378+Adam-Lnenicka@users.noreply.github.com>
2020-10-23 11:18:44 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
     <h2>{{ $pet->id }}</h2>
     <h2>{{ $pet->name }}</h2>
     <h2>{{ $pet->breed }}</h2>
+    <h2>{{ $pet->weight }}</h2>
+    <h2>{{ $pet->species }}</h2>
     <h2>{{ $pet->age }}</h2>
 @endforeach
 
