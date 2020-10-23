9a1c456fdb308d92878156cf3376790f72fa5f83
Hilenn <hilenna@seznam.cz>
2020-10-23 12:32:23 +0200

    was not able to switch back to my branch until ths was commited

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 class OwnerController extends Controller
 {
     //
+
+    public function  show($id){
+
+        // $owner = $request->input('id');
+
+        //use model to get id from database 
+
+       $owner =Owner::findOrFail($id);
+
+
+
+        return view('owners/owner', compact('owner'));
+    }
 }
---
+++ resources/views/owners/owner.blade.php
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+<h1>Owner Name</h1>
+<p>Owner image </p>
+
+
+<p>Pet Name</p>
+<p>Phone number</p>
+<p>Address</p>
+<p>Email Address</p>
