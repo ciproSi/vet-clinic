ad5d2154a56df2791e88e03145ea258e69eca769
radamusil <64097743+radamusil@users.noreply.github.com>
2020-10-23 12:59:44 +0200

    part of owners index

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Owner;
 
 class OwnerController extends Controller
 {
-    //
+    public function index()
+    {
+        
+        $owners = Owner::with('pets')->get();
+
+        return view('owners/index', compact('owners'));
+    }
 }
